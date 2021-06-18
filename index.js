26caad2adbcfef925d127301c976ea438e6319d7
Santanu Sinha <santanu.sinha@gmail.com>
2021-06-18 14:11:30 +0530

    Moved details to config and added comments

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const express = require('express')
-const { promisify } = require('util')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const refresh = require('passport-oauth2-refresh')
-const OAuth2Strategy = require('passport-oauth').OAuth2Strategy
-const cookieSession = require('cookie-session')
-const NodeCache = require('node-cache')
+const express = require('express');
+const { promisify } = require('util');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const refresh = require('passport-oauth2-refresh');
+const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
+const cookieSession = require('cookie-session');
+const NodeCache = require('node-cache');
 const morgan = require('morgan');
+const config = require('./config.json');
 
-const authCache = new NodeCache({
-    stdTTL: 60,
-    checkPeriod: 90
-});
-
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
+//We shall be using session cookies to store idman token
 app.use(cookieSession({
     maxAge: 60 * 60 * 1000,
     keys: [ 'testkey' ]
 }));
 
+// Create a local cache to store authntication information temporarily
+// Don't make this too long ... the longer it is the risks are higher
+// Too short times, means you will do remote verification more frequently, affecting your app performance
+const authCache = new NodeCache({
+    stdTTL: 60,
+    checkPeriod: 90
+});
+
+//Passport oauth2 strategy init. Variables come from config.json
 const strategy = new OAuth2Strategy({
-    authorizationURL: 'http://1c36f2992593.ngrok.io/apis/oauth2/authorize',
-    tokenURL: 'http://1c36f2992593.ngrok.io/apis/oauth2/token',
-    clientID: 'NODE_TEST',
-    clientSecret: '5c1f367d-1250-4396-a6fb-c5c93293efbb',
+    authorizationURL: config.idman.endpoint + '/apis/oauth2/authorize',
+    tokenURL: config.idman.endpoint + '/apis/oauth2/token',
+    clientID: config.idman.clientId,
+    clientSecret: config.idman.secret,
     callbackURL: "http://localhost:3000/callback"
   },
   function(accessToken, refreshToken, params, profile, cb) {
@@ -35,14 +41,19 @@ const strategy = new OAuth2Strategy({
   }
 );
 
+//Same strategy is used for getting token and refreshing it
 passport.use('oauth2', strategy);
 refresh.use('oauth2', strategy);
 
+
+// Once user is received from authorise, you can set it in cache
 passport.serializeUser((data, cb) => {
     authCache.set(data.accessToken, data.user);
     cb(null, data.accessToken);
 });
 
+//Get and return the user, if not present in cache, refresh the token to get the data back
+//This will also check the token for validity
 passport.deserializeUser((accessToken, cb) => {
     if(!accessToken) {
         return cb(null, null);
@@ -52,7 +63,8 @@ passport.deserializeUser((accessToken, cb) => {
         return cb(null, user);
     }
     else {
-    refresh.requestNewAccessToken(
+        //Refresh the token
+        refresh.requestNewAccessToken(
                 'oauth2',
                 accessToken,
                 (err, accessToken, refreshToken, params) => {
@@ -68,14 +80,19 @@ passport.deserializeUser((accessToken, cb) => {
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Check if request is contains required auth. You can also do roll check here if you want
 const checkAuth = (req, res, next) => req.user ? next() : res.redirect('/login')
 
+//This is a secure resource
 app.get('/', checkAuth, (req, res) => {
   res.send('Hello World! ' + JSON.stringify(req.user));
 });
 
+//This will initiate call to idman server and redirect user to login screen
 app.get('/login', passport.authenticate('oauth2'));
 
+//This will receive the token id from idman and make backend call to get token from idman
+//It will save token to session cookie to be used in browser
 app.get('/callback', passport.authenticate('oauth2',
                         { successRedirect: '/', failureRedirect: '/login' }));
 
